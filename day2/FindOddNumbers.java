b8a345a140d1509c850b93debb8519c2faf46c2a
RESHMA <161284805+RESH10112002@users.noreply.github.com>
2024-09-19 11:44:14 +0530

    Add files via upload
    Week1:Day1&Day2

---
+++ day2/FindOddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		for(int i=0;i<=10;i++) {
+			if(i%2==1) {
+				System.out.println("The odd numbers are:"+i);
+			}
+		}
+
+	}
+
+}
